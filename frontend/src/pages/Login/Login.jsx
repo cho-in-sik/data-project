d6f8ed604523c69878d58dc5a5102966ff1a3acb
Anjgmn <jermee315@gmail.com>
2023-03-14 13:23:36 +0900

    feat: join, login 2차

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
-// import axios from 'axios';
+import axios from 'axios';
 import { useNavigate, Link } from 'react-router-dom';
+import styled from 'styled-components';
+import BackGround from '../../components/Background/Background';
+import Header from '../../components/Header/Header';
 
 function Login() {
   const navigate = useNavigate();
@@ -22,34 +25,116 @@ function Login() {
     }
     alert('로그인에 성공했습니다.');
     navigate('/MyPage');
-    /* try {
-      const res = await axios.post('/api/login', { email, pw });
+    try {
+      const res = await axios.post('http://localhost:3000/api/v1/users', {
+        email,
+        pw,
+      });
       console.log(res);
       localStorage.setItem('token', res.data.token);
       navigate('/MyPage');
     } catch (e) {
       console.log(e);
-    } */
+    }
   };
   return (
-    <div className="LoginBox">
-      <h1 className="LoginTitle">로그인</h1>
-      <div className="LoginWrapper">
-        <p>이메일</p>
-        <input
-          type="email"
-          name="email"
-          value={email}
-          onChange={handleEmailChange}
-        />
-        <p>비밀번호</p>
-        <input type="text" name="pw" value={pw} onChange={handlePwChange} />
-      </div>
-      <button onClick={handleClick}>로그인</button>
-      <p>아직 회원이 아니신가요?</p>
-      <Link to="/join">회원가입하기</Link>
-    </div>
+    <BackGround>
+      <Header />
+      <LoginBox>
+        <LoginTitle>로그인</LoginTitle>
+        <LoginWrapper>
+          <LoginItem>
+            <p>이메일</p>
+            <input
+              type="email"
+              name="email"
+              value={email}
+              onChange={handleEmailChange}
+            />
+          </LoginItem>
+          <LoginItem>
+            <p>비밀번호</p>
+            <input type="text" name="pw" value={pw} onChange={handlePwChange} />
+          </LoginItem>
+        </LoginWrapper>
+        <button onClick={handleClick}>로그인</button>
+        <p>아직 회원이 아니신가요?</p>
+        <Link to="/join">회원가입하기</Link>
+      </LoginBox>
+    </BackGround>
   );
 }
 
 export default Login;
+
+const LoginBox = styled.div`
+  width: 30%;
+  min-width: 520px;
+  background: #eee;
+  border-radius: 20px;
+  text-align: center;
+  padding: 3rem 2rem;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  margin-top: 3rem;
+
+  button {
+    display: block;
+    border: none;
+    color: #fff;
+    font-size: 1.2rem;
+    font-weight: bold;
+    background-color: #47b781;
+    width: 20rem;
+    height: 2.5rem;
+    border-radius: 20px;
+    margin: 1rem auto 0.5rem;
+    cursor: pointer;
+  }
+  p {
+    display: inline-block;
+    font-size: 0.8rem;
+    margin-right: 5rem;
+  }
+  a {
+    text-decoration: none;
+    color: #ff0000;
+    font-size: 0.8rem;
+  }
+`;
+
+const LoginTitle = styled.h1`
+  width: 50%;
+  font-size: 2rem;
+  margin: 1rem auto;
+`;
+
+const LoginWrapper = styled.div`
+  width: 70%;
+  margin: 0 auto;
+`;
+
+const LoginItem = styled.div`
+  width: 90%;
+  margin: 2rem 1rem;
+  p {
+    font-size: 0.8rem;
+    text-align: left;
+    width: 30%;
+    margin-bottom: 0.5rem;
+    float: left;
+  }
+  input {
+    width: 100%;
+    background-color: #eee;
+    border: none;
+    border-bottom: 2px solid #a3a3a3;
+    font-size: 1.2rem;
+    margin-bottom: 0.3rem;
+  }
+  input:focus {
+    outline: none;
+  }
+  input::placeholder {
+    font-size: 0.8rem;
+  }
+`;
