acb392e043316560e45e6553c4881e1c1dde835d
Junghyun <ddongzang2@gmail.com>
2023-03-15 19:13:45 +0900

    feat : 게시글 목록 Pagination 기능 구현
    css 적용 전입니다.

--- frontend/src/pages/Community/CommunityList.jsx
+++ frontend/src/pages/Community/CommunityList.jsx
@@ -17,24 +17,34 @@ import {
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUsers } from '@fortawesome/free-solid-svg-icons';
 import Header from '../../components/Header/Header';
+import Pagination from '../../components/Pagination/Pagination';
 
 function CommunityList() {
-  const [boards, setBoards] = useState([]);
+  const [boards, setBoards] = useState([]); //게시판 목록
+  const [page, setPage] = useState(1); //현재 페이지
+  const perPage = 5; //한 페이지에 보여줄 게시글 수
 
+  // 게시글 목록을 가져오는 함수
   useEffect(() => {
     async function fetchData() {
       try {
         const response = await axios.get(
-          'http://localhost:3000/api/v1/board/all',
+          `http://localhost:3000/api/v1/board?page=${page}&perPage=${perPage}`,
         );
-        // 가져온 데이터를 setBoards 함수를 사용하여 boards 상태에 저장
-        setBoards(response.data);
+        // 응답 데이터에서 boards 배열만 추출하여 setBoards로 업데이트
+        setBoards(response.data.boards);
+        // console.log(response.data.boards);
       } catch (error) {
         console.log(error);
       }
     }
     fetchData();
-  }, []);
+  }, [page, perPage]); //page, perPage가 변경될 때마다 useEffect가 실행
+
+  // 페이지 번호를 변경하는 함수
+  const handlePageChange = (pageNumber) => {
+    setPage(pageNumber);
+  };
 
   return (
     <>
@@ -52,7 +62,6 @@ function CommunityList() {
             </CommunityListTableHeaderAuthor>
           </CommunityListTableHeader>
           <CommunityListTableBody>
-            {/* 게시물이 존재할 경우 */}
             {boards && boards.length > 0 ? (
               boards.map((board) => (
                 <CommunityListTableRow key={board._id}>
@@ -67,7 +76,6 @@ function CommunityList() {
                 </CommunityListTableRow>
               ))
             ) : (
-              // 게시물이 존재하지 않을 경우
               <CommunityListTableRow>
                 <CommunityListTableTitle>
                   게시글이 없습니다.
@@ -77,6 +85,7 @@ function CommunityList() {
             )}
           </CommunityListTableBody>
         </CommunityListTable>
+        <Pagination page={page} handlePageChange={handlePageChange} />
         <WriteButton to="/board">게시글 작성하기</WriteButton>
       </Container>
     </>
