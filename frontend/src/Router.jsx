91a2d958c2b826c4594290395d8fe198752ec313
Anjgmn <jermee315@gmail.com>
2023-03-16 17:19:44 +0900

    feat: admin 프라이빗 라우터 설정

--- frontend/src/Router.jsx
+++ frontend/src/Router.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
 import App from './App';
 import Join from './pages/Join/Join';
 import Login from './pages/Login/Login';
@@ -10,16 +13,19 @@ import CommunityList from './pages/Community/CommunityList';
 import CommunityDetail from './pages/Community/CommunityDetail';
 import CommunityForm from './pages/Community/CommunityForm';
 import CommunityEdit from './pages/Community/CommunityEdit';
+import MyVolunteers from './pages/MyVolunteer/MyVolunteers';
+import VolunteerDetail from './pages/MyVolunteer/VolunteerDetail';
+
+// 관리자 페이지
+import AdminRoute from './components/AdminRoute/AdminRoute';
 import AdminMain from './pages/Admin/AdminMain';
 import AdminUser from './pages/Admin/AdminUser';
 import AdminVolunteer from './pages/Admin/AdminVolunteer';
 import AdminCommunity from './pages/Admin/AdminCommunity';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
-import MyVolunteers from './pages/MyVolunteer/MyVolunteers';
-import VolunteerDetail from './pages/MyVolunteer/VolunteerDetail';
 
 export function Router() {
+  const user = useSelector((state) => state.user);
+
   return (
     <BrowserRouter>
       <Routes>
@@ -36,10 +42,12 @@ export function Router() {
         <Route path="/board/:id" element={<CommunityDetail />} />
         <Route path="/board/edit/:id" element={<CommunityEdit />} />
         <Route path="/board" element={<CommunityForm />} />
-        <Route path="/admin" element={<AdminMain />} />
-        <Route path="/admin/user" element={<AdminUser />} />
-        <Route path="/admin/volunteer" element={<AdminVolunteer />} />
-        <Route path="/admin/community" element={<AdminCommunity />} />
+        <Route element={<AdminRoute user={user} />}>
+          <Route path="/admin" element={<AdminMain />} />
+          <Route path="/admin/user" element={<AdminUser />} />
+          <Route path="/admin/volunteer" element={<AdminVolunteer />} />
+          <Route path="/admin/community" element={<AdminCommunity />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
